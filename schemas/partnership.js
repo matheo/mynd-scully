32c020df4288ffa6b04124e439bdc51fe924a75e
Mateo Tibaquira <nestormateo@gmail.com>
2021-04-01 02:24:07 -0500

    build: sanity init

---
+++ schemas/partnership.js
@@ -0,0 +1,51 @@
+export default {
+  name: 'partnership',
+  title: 'Partnership',
+  type: 'document',
+  fields: [
+    {
+      name: 'partner',
+      title: 'Partner',
+      type: 'string',
+    },
+    {
+      name: 'slug',
+      title: 'Slug',
+      type: 'slug',
+      options: {
+        source: 'name',
+        maxLength: 96,
+      },
+    },
+    {
+      name: 'logo',
+      title: 'Logo',
+      type: 'image',
+      options: {
+        hotspot: true,
+      },
+    },
+    {
+      name: 'heading',
+      title: 'heading',
+      type: 'text',
+    },
+    {
+      name: 'content',
+      title: 'Content',
+      type: 'text',
+    },
+    {
+      name: 'publishedAt',
+      title: 'Published at',
+      type: 'datetime',
+    },
+  ],
+
+  preview: {
+    select: {
+      title: 'partner',
+      media: 'logo',
+    },
+  },
+}
