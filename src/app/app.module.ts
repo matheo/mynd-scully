2bce2f3e0f70511ab051e8d9583296895b559285
Mateo Tibaquira <nestormateo@gmail.com>
2021-04-01 02:53:09 -0500

    build: ng add @scullyio/init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { ScullyLibModule } from '@scullyio/ng-lib';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -10,6 +11,7 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    ScullyLibModule,
     AppRoutingModule
   ],
   providers: [],
